65f64617754ab524728ab37566d58837dc3cf230
Monster <Monster@DESKTOP-9JCHGP7>
2023-04-28 10:10:36 +0300

    Add project files.

---
+++ DataAccessLAyer/Abstract/ICategoryDal.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLAyer.Abstract
+{
+    public  interface ICategoryDal:IRepository<Category>
+    {
+        //CRUD 
+        //Type Name();
+        List<Category> List();
+        void Insert(Category p);
+        void Update(Category p);
+        void Delete(Category p);
+       
+
+    }
+}
